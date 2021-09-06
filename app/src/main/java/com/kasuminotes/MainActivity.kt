7739d6994047719b9d85fe7174e487c8753be583
chilbi <13232158717@163.com>
2021-09-06 23:05:56 +0800

    suppress warning

--- app/src/main/java/com/kasuminotes/MainActivity.kt
+++ app/src/main/java/com/kasuminotes/MainActivity.kt
@@ -19,13 +19,14 @@ import com.kasuminotes.ui.app.App
 import com.kasuminotes.utils.LocaleUtil
 import com.kasuminotes.utils.ProvideImageLoader
 
-@ExperimentalCoilApi
-@ExperimentalAnimationApi
-@ExperimentalMaterialApi
-@ExperimentalFoundationApi
 class MainActivity : ComponentActivity() {
     private var broadcastReceiver: BroadcastReceiver? = null
 
+    @Suppress("EXPERIMENTAL_ANNOTATION_ON_OVERRIDE_WARNING")
+    @ExperimentalCoilApi
+    @ExperimentalAnimationApi
+    @ExperimentalMaterialApi
+    @ExperimentalFoundationApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         instance = this
