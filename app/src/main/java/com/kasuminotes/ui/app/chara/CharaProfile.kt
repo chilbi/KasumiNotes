dc3e5a6dad48fe4972452d3aac8a738abb77e4fd
chilbi <13232158717@163.com>
2022-05-07 16:30:04 +0800

    update charaProfile

--- app/src/main/java/com/kasuminotes/ui/app/chara/CharaProfile.kt
+++ app/src/main/java/com/kasuminotes/ui/app/chara/CharaProfile.kt
@@ -7,7 +7,6 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.verticalScroll
