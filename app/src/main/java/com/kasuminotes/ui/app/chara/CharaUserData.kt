be22f6e61fb46eda39b94a1e994966a71749c8ec
chilbi <13232158717@163.com>
2023-04-10 04:30:53 +0800

    update item_00000.png

--- app/src/main/java/com/kasuminotes/ui/app/chara/CharaUserData.kt
+++ app/src/main/java/com/kasuminotes/ui/app/chara/CharaUserData.kt
@@ -17,6 +17,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -214,7 +215,7 @@ private fun RankBonusText(
 ) {
     Text(
         text = text,
-        color = if (hasBonus) MaterialTheme.colorScheme.onSurface else ShadowColor,
+        color = if (hasBonus) Color.DarkGray else ShadowColor,
         style = MaterialTheme.typography.labelSmall
     )
 }
