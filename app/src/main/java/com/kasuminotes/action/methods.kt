1f3ca6ab236d3041408f932b7d05378c428a0fb0
chilbi <13232158717@163.com>
2023-10-01 01:11:07 +0800

    update skill

--- app/src/main/java/com/kasuminotes/action/methods.kt
+++ app/src/main/java/com/kasuminotes/action/methods.kt
@@ -223,6 +223,7 @@ fun getStateContent(detail: Int, actionId: Int): D {
         135 -> D.Format(R.string.state_battery)
         137 -> D.Format(R.string.state_kairai)
         139 -> D.Format(R.string.state_roar_of_wolf_tooth)
+        140 -> D.Format(R.string.state_fire_dance_seal)
         else -> D.Format(R.string.state_unknown, arrayOf(D.Text(detail.toString())))
     }
 }
