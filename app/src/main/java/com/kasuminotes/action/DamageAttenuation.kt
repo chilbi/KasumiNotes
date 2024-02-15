8d63a07480b3cb4547ced79940f0c5bd86b89e51
chilbi <13232158717@163.com>
2024-02-15 21:36:33 +0800

    update skill

--- app/src/main/java/com/kasuminotes/action/DamageAttenuation.kt
+++ app/src/main/java/com/kasuminotes/action/DamageAttenuation.kt
@@ -2,21 +2,29 @@ package com.kasuminotes.action
 
 import com.kasuminotes.R
 import com.kasuminotes.data.SkillAction
+import kotlin.math.floor
 import kotlin.math.ln
 
 fun SkillAction.getDamageAttenuation(): D {
+    val limit = actionValue5 / 10000
+    val n4 = if (limit > 100.0) limit + 100.0 else floor(limit / 100.0) + 100.0
+    val n6 = n4 + 100.0
+    val n8 = 999.0
     val s = actionValue5 / 10000
     val m = actionValue1 / 10000
 
     return D.Format(
-        R.string.action_damage_attenuation_target1_limit2_time3_r4_r5_r6,
+        R.string.action_damage_attenuation_target1_limit2_time3_n4_r5_n6_r7_n8_r9,
         arrayOf(
             getTarget(depend),
-            D.Text((actionValue5 / 10000).toNumStr()),
+            D.Text(limit.toNumStr()),
             D.Text(actionValue3.toNumStr()),
-            getDamageText(100.0, s, m),
-            getDamageText(200.0, s, m),
-            getDamageText(999.0, s, m)
+            D.Text(n4.toNumStr()),
+            getDamageText(n4, s, m),
+            D.Text(n6.toNumStr()),
+            getDamageText(n6, s, m),
+            D.Text(n8.toNumStr()),
+            getDamageText(n8, s, m)
         )
     )
 }
