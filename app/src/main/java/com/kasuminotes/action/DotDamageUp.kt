60d3e09aa400e364ade297f4c06db75ce59af9e5
chilbi <13232158717@163.com>
2024-04-23 18:13:16 +0800

    update skill

--- app/src/main/java/com/kasuminotes/action/DotDamageUp.kt
+++ app/src/main/java/com/kasuminotes/action/DotDamageUp.kt
@@ -4,9 +4,9 @@ import com.kasuminotes.R
 import com.kasuminotes.data.SkillAction
 
 fun SkillAction.getDotDamageUp(): D {
-    val content = if (actionValue1 == 400.0) {
+    val content = if (actionValue3 == -1.0) {
         D.Format(R.string.content_dot)
-    } else {//9900.0
+    } else {
         var dot: D? = null
         arrayOf(actionValue3, actionValue4, actionValue5, actionValue6).forEach { value ->
             if (value > -1.0) {
@@ -20,13 +20,14 @@ fun SkillAction.getDotDamageUp(): D {
                 }
             }
         }
-        if (dot != null) dot!! else D.Unknown
+        if (dot != null) dot!! else D.Format(R.string.content_dot)
     }
     return D.Format(
-        R.string.action_dot_damage_up_target1_content2_formula3_time4,
+        R.string.action_dot_damage_up_target1_content2_formula3_max4_time5,
         arrayOf(
             getTarget(depend),
             content,
+            D.Text("${actionValue1.toNumStr()}%"),
             D.Text(actionValue7.toNumStr()),
             D.Text(actionValue2.toNumStr())
         )
