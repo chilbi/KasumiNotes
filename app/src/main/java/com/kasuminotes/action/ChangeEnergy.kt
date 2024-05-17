0b7a30928f4da6a6015cbbff376237a81a32bf19
chilbi <13232158717@163.com>
2024-05-17 20:12:13 +0800

    update skill

--- app/src/main/java/com/kasuminotes/action/ChangeEnergy.kt
+++ app/src/main/java/com/kasuminotes/action/ChangeEnergy.kt
@@ -5,8 +5,11 @@ import com.kasuminotes.data.SkillAction
 
 fun SkillAction.getChangeEnergy(skillLevel: Int): D {
     return D.Format(
-        if (actionDetail1 == 1) R.string.action_energy_recovery_target1_formula2
-        else R.string.action_energy_down_target1_formula2,
+        when (actionDetail1) {
+            1 -> R.string.action_energy_recovery_target1_formula2
+            4 -> R.string.action_energy_recovery_fixed_target1_formula2
+            else -> R.string.action_energy_down_target1_formula2
+        },
         arrayOf(
             getTarget(depend),
             getBaseLvFormula(actionValue1, actionValue2, skillLevel)
