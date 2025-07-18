871a33d151b64f6098b92b110dfad08c3ec26759
chilbi <13232158717@163.com>
2025-07-18 21:05:23 +0800

    fix bug ExtraEffectData

--- app/src/main/java/com/kasuminotes/db/enemy.kt
+++ app/src/main/java/com/kasuminotes/db/enemy.kt
@@ -1,5 +1,6 @@
 package com.kasuminotes.db
 
+import android.database.sqlite.SQLiteException
 import com.kasuminotes.data.EnemyData
 import com.kasuminotes.data.ExtraEffectData
 import com.kasuminotes.data.Property
@@ -128,38 +129,42 @@ WHERE ep.enemy_id IN (${multiParts.joinToString(",")})"""
 fun AppDatabase.getExtraEffectData(targetId: Int, epTableName: String): ExtraEffectData? {
     if (!existsTable("extra_effect_data")) return null
 
-    val sql = """SELECT ${ExtraEffectData.getFields()}
+    try {
+        val sql = """SELECT ${ExtraEffectData.getFields()}
 FROM extra_effect_data AS eed
 JOIN $epTableName AS ep ON ep.enemy_id in (eed.enemy_id_1,eed.enemy_id_2,eed.enemy_id_3,eed.enemy_id_4,eed.enemy_id_5)
 WHERE $targetId IN (eed.target_value_1,eed.target_value_2)"""
 
-    return useDatabase {
-        rawQuery(sql, null).use {
-            if (it.moveToFirst()) {
-                var i = 0
-                val targetValueList = mutableListOf<Int>()
-                while (i < 2) {
-                    targetValueList.add(it.getInt(i++))
+        return useDatabase {
+            rawQuery(sql, null).use {
+                if (it.moveToFirst()) {
+                    var i = 0
+                    val targetValueList = mutableListOf<Int>()
+                    while (i < 2) {
+                        targetValueList.add(it.getInt(i++))
+                    }
+                    val enemyIdList = mutableListOf<Int>()
+                    while (i < 7) {
+                        enemyIdList.add(it.getInt(i++))
+                    }
+                    val execTimingList = mutableListOf<Int>()
+                    while (i < 12) {
+                        execTimingList.add(it.getInt(i++))
+                    }
+                    ExtraEffectData(
+                        it.getInt(i++),
+                        it.getInt(i++),
+                        it.getInt(i),
+                        targetValueList,
+                        enemyIdList,
+                        execTimingList
+                    )
+                } else {
+                    null
                 }
-                val enemyIdList = mutableListOf<Int>()
-                while (i < 7) {
-                    enemyIdList.add(it.getInt(i++))
-                }
-                val execTimingList = mutableListOf<Int>()
-                while (i < 12) {
-                    execTimingList.add(it.getInt(i++))
-                }
-                ExtraEffectData(
-                    it.getInt(i++),
-                    it.getInt(i++),
-                    it.getInt(i),
-                    targetValueList,
-                    enemyIdList,
-                    execTimingList
-                )
-            } else {
-                null
             }
         }
+    } catch (_: SQLiteException) {
+        return null
     }
 }
