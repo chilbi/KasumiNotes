8237f1f9978be3ad446379ef615f4fd2061f0cae
chilbi <13232158717@163.com>
2025-09-18 23:50:39 +0800

    update skill

--- app/src/main/java/com/kasuminotes/action/HitCount.kt
+++ app/src/main/java/com/kasuminotes/action/HitCount.kt
@@ -4,7 +4,7 @@ import com.kasuminotes.R
 import com.kasuminotes.data.SkillAction
 
 fun SkillAction.getHitCount(): D {
-    return D.Format(
+    val result = D.Format(
         R.string.action_hit_count_target1_time2_count3,
         arrayOf(
             getTarget(depend),
@@ -12,4 +12,14 @@ fun SkillAction.getHitCount(): D {
             D.Text(actionValue1.toNumStr()).style(primary = true, bold = true)
         )
     )
+    return if (actionValue5 > 0) {
+        result.append(
+            D.Format(
+                R.string.action_hit_count_max1,
+                arrayOf(D.Text(actionValue5.toNumStr()).style(primary = true, bold = true))
+            )
+        )
+    } else {
+        result
+    }
 }
