cc0a28e20484912cdded8755f4e50140bf08a744
chilbi <13232158717@163.com>
2026-01-31 12:29:26 +0800

    update skill

--- app/src/main/java/com/kasuminotes/action/Dispel.kt
+++ app/src/main/java/com/kasuminotes/action/Dispel.kt
@@ -8,7 +8,28 @@ fun SkillAction.getDispel(): D {
         1, 3 -> D.Format(R.string.content_status_up_effect)
         2 -> D.Format(R.string.content_status_down_effect)
         10 -> D.Format(R.string.content_barrier)
-        20 -> D.Format(R.string.content_magic_def_down_effect)
+        20 -> {
+            val effectValues = listOf(actionValue4, actionValue5, actionValue6, actionValue7).filter { it != 0.0 }
+            if (effectValues.isNotEmpty()) {
+                val effects: List<D> = effectValues.map {
+                    val detail1 = it.toInt()
+                    D.Join(arrayOf(
+                        getStatusContent(detail1 / 10),
+                        D.Format(if (detail1 % 10 == 0) R.string.content_up else R.string.content_down)
+                    ))
+                }
+                val list = mutableListOf<D>()
+                effects.forEachIndexed { index, effect ->
+                    if (index != 0) {
+                        list.add(D.Format(R.string.comma))
+                    }
+                    list.add(effect)
+                }
+                D.Format(R.string.content_effect1, arrayOf(D.Join(list.toTypedArray())))
+            } else {
+                D.Unknown
+            }
+        }
         else -> D.Unknown
     }
 
